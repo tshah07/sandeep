6bcb5411fc0f4050e476a31ceee8e469334c92f5
Tejas Shah <tshah.work@gmail.com>
2013-04-07 16:40:45 -0400

    at middle of working dojo dgrid

--- application/views/grid.php
+++ application/views/grid.php
@@ -9,35 +9,17 @@
 
 <script>
 	require(["dgrid/Grid", "dojo/domReady!"], function(Grid) {
-		var data = [{
-			first : "Bob",
-			last : "Barker",
-			age : 89
-		}, {
-			first : "Vanna",
-			last : "White",
-			age : 55
-		}, {
-			first : "Pat",
-			last : "Sajak",
-			age : 65
-		}];
+	var data = <?php echo json_encode($gridData); ?>
+	;
 
-		var grid = new Grid({
-			columns : {
-				first : "First Name",
-				last : "Last Name",
-				age : "Age"
-			}
-		}, "grid");
-		grid.renderArray(data);
-	}); 
+	var grid = new Grid({
+		columns : {
+			firstName : "First Name",
+			lastName : "Last Name",
+			email : "Email"
+		}
+	}, "grid");
+	grid.renderArray(data);
+	});
 </script>
 <div id="grid"></div>
-<?php
-
-foreach ($gridData as $row) {
-
-	echo $row['firstName'] . "<br>";
-}
-?>
